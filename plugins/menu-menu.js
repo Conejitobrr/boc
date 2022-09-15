4e5a331f35f5c67f31e6af1900ec6937a3209824
Conejitobrr <110344521+Conejitobrr@users.noreply.github.com>
2022-09-15 17:42:52 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -487,7 +487,6 @@ externalAdReply: {
 showAdAttribution: true,
 mediaType: 'VIDEO',
 mediaUrl: null,
-title: '👑 𝐓𝐇𝐄 𝐌𝐘𝐒𝐓𝐈𝐂 - 𝐁𝐎𝐓 👑',
 body: null,
 }}}
 conn.sendMessage(m.chat, buttonMessage, { quoted: m })
